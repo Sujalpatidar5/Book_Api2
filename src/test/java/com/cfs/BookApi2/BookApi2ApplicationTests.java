4cb8f7c22c8002f1a37bf63dece1a19b85918d28
Sujal Patidar <patidarsujal616@gmail.com>
2026-02-24 21:52:51 +0530

    Initial commit

---
+++ src/test/java/com/cfs/BookApi2/BookApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfs.BookApi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
